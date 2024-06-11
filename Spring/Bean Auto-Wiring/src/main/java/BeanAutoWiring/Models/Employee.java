66639e59152c90d0223b6ff376d2748e3e193d44
“ankitaghadge19” <ghadge.ankita197@gmail.com>
2024-06-11 23:15:47 +0530

    11-06-2024

--- Spring/Bean Auto-Wiring/src/main/java/BeanAutoWiring/Models/Employee.java
+++ Spring/Bean Auto-Wiring/src/main/java/BeanAutoWiring/Models/Employee.java
@@ -2,18 +2,29 @@ package BeanAutoWiring.Models;
 
 public class Employee {
     private String fullName;
-
     private Department department;
+
+    // Default constructor
+    public Employee() {
+    }
+
+    // Constructor for constructor-based autowiring
+    public Employee(Department department) {
+        this.department = department;
+    }
 
     public Department getDepartment() {
         return department;
     }
+
     public void setDepartment(Department department) {
         this.department = department;
     }
+
     public String getFullName() {
         return fullName;
     }
+
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
