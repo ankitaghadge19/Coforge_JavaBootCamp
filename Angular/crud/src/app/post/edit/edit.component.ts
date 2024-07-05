b985e69ffd45477823763f85470aff848403e7fb
“ankitaghadge19” <ghadge.ankita197@gmail.com>
2024-07-05 09:18:37 +0530

    05-07-2024

---
+++ Angular/crud/src/app/post/edit/edit.component.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from '../post';
+import {
+  FormGroup,
+  ReactiveFormsModule,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-edit',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './edit.component.html',
+  styleUrl: './edit.component.css',
+})
+export class EditComponent {
+  id!: number;
+  post!: Post;
+  form!: FormGroup;
+  constructor(
+    public postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['postId'];
+    this.postService.find(this.id).subscribe((data: Post) => {
+      this.post = data;
+    });
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      body: new FormControl('', Validators.required),
+    });
+  }
+  get f() {
+    return this.form.controls;
+  }
+  submit() {
+    console.log(this.form.value);
+    this.postService.update(this.id, this.form.value).subscribe((res: any) => {
+      console.log('Post Successfully Updated');
+      this.router.navigateByUrl('post/index');
+    });
+  }
+}
---
+++ Angular/crud/src/app/post/post.service.ts
@@ -0,0 +1,65 @@
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Post } from './post';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  private apiURL = 'https://jsonplaceholder.typicode.com/posts';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  constructor(private httpClient: HttpClient) {}
+
+  getAll(): Observable<any> {
+    return this.httpClient
+      .get(this.apiURL, this.httpOptions)
+      .pipe(catchError(this.errorHandler));
+  }
+  create(post: Post): Observable<any> {
+    return this.httpClient
+      .post(this.apiURL + '/posts/', JSON.stringify(post), this.httpOptions)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  find(id: number): Observable<any> {
+    return this.httpClient
+      .get(this.apiURL + '/posts/' + id)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  update(id: number, post: Post): Observable<any> {
+    return this.httpClient
+      .put(this.apiURL + '/posts/' + id, JSON.stringify(post), this.httpOptions)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  delete(id: number) {
+    return this.httpClient
+      .delete(this.apiURL + '/posts/' + id, this.httpOptions)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  private errorHandler(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
+  }
+}
